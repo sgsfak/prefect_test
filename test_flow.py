51d7735be95d41a821763fe4f5356ecfe649b7ba
Stelios G. Sfakianakis <sgsfak@gmail.com>
2024-07-11 00:56:53 +0300

    add flow

---
+++ test_flow.py
@@ -0,0 +1,15 @@
+from prefect import flow, task
+
+
+@task
+def print_hello(name):
+    print(f"Hello {name}!")
+
+
+@flow(name="Hello Flow")
+def hello_world(name="world"):
+    print_hello(name)
+
+
+if __name__ == "__main__":
+    hello_world.serve(name="prefect-docker-guide")
